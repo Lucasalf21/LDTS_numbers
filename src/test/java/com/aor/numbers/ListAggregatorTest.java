f299af143f4e38ee49c193c1a6de27bc1c568542
Lucas Alfonsi <lucasbranquinhoalf@gmail.com>
2023-12-20 15:00:53 +0000

    fixed distinct_bug_8726

--- src/test/java/com/aor/numbers/ListAggregatorTest.java
+++ src/test/java/com/aor/numbers/ListAggregatorTest.java
@@ -3,6 +3,7 @@ package com.aor.numbers;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import java.util.Arrays;
 import java.util.List;
@@ -47,7 +48,11 @@ public class ListAggregatorTest {
     public void distinct() {
 
         ListAggregator aggregator = new ListAggregator();
-        int distinct = aggregator.distinct(list);
+
+        GenericListDeduplicator deduplicator = Mockito.mock(GenericListDeduplicator.class);
+        Mockito.when(deduplicator.deduplicate(Mockito.anyList())).thenReturn(Arrays.asList(1, 2, 4, 5));
+
+        int distinct = aggregator.distinct(list, deduplicator);
 
         Assertions.assertEquals(4, distinct);
     }
@@ -59,5 +64,17 @@ public class ListAggregatorTest {
         int max = aggregator.max(list);
 
         Assertions.assertEquals(-1, max);
+    }
+
+    @Test
+    public void distinct_bug_8726() {
+        ListAggregator aggregator = new ListAggregator();
+
+        GenericListDeduplicator deduplicator = Mockito.mock(GenericListDeduplicator.class);
+        Mockito.when(deduplicator.deduplicate(Mockito.anyList())).thenReturn(Arrays.asList(1, 2, 4));
+
+        int distinct = aggregator.distinct(Arrays.asList(1, 2, 4, 2), deduplicator);
+
+        Assertions.assertEquals(3, distinct);
     }
 }
